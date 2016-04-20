8002b6909d973ea529aaef9e18c0275a95fd6234
rdab <rdamian.alfonso@riseup.net>
2016-04-20 16:05:22 -0400

    Refactored code to use new URL for inserting new items

--- superlists/urls.py
+++ superlists/urls.py
@@ -5,8 +5,7 @@ from django.conf.urls import patterns, url  # include
 urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
-        name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+        name='view_list'),
+    url(r'^lists/new$', 'lists.views.new_list', name='new_list')
     # url(r'^admin/', include(admin.site.urls)),
 )
